a59b9b3fcd7f4b17d00fe356f00f2135ef1eb790
Zeyu Su <suz15@miamioh.edu>
2022-12-08 21:53:25 -0500

    Update customer

--- Bank/src/Customer.java
+++ Bank/src/Customer.java
@@ -1,12 +1,14 @@
 import java.util.*;
 
 public class Customer {
-    private static String customerName;
-    private static int customerId;
+    private String customerName;
+    private String pwd;
+    private int customerId;
     private ArrayList<Account> accounts;
 
-    public Customer(String name, int id){
+    public Customer(String name, String pwd, int id){
         customerName = name;
+        this.pwd = pwd;
         customerId = id;
         accounts = new ArrayList<>();
     }
