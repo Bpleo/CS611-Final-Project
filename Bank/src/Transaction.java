8c0e7182314ace74684b6862126dd93beff808e6
Zeyu Su <suz15@miamioh.edu>
2022-12-09 21:47:51 -0500

    Update transaction, accounts' tostring needs improve

--- Bank/src/Transaction.java
+++ Bank/src/Transaction.java
@@ -2,7 +2,23 @@ public class Transaction {
     private Account senderAccount;
     private Account receiverAccount;
     private double sendAmount;
-    public void transferAmount(int accountId, double amount){
+    private CurrencyType sendType;
+    private CurrencyType recvType;
 
+    public Transaction(Account senderAccount, Account receiverAccount, double sendAmount, CurrencyType sendType, CurrencyType recvType){
+         senderAccount.withdraw(sendType,sendAmount);
+         double amount = Exchange.exchangeCurrency(sendType,recvType,sendAmount);
+         receiverAccount.deposit(recvType, amount);
+         this.senderAccount = senderAccount;
+         this.receiverAccount = receiverAccount;
+         this.sendAmount = sendAmount;
+         this.sendType = sendType;
+         this.recvType = recvType;
+         Report.addTransaction(this);
+    }
+
+    @Override //need to specify account toString
+    public String toString(){
+        return senderAccount + " sends " + sendAmount + sendType + " to " + receiverAccount;
     }
 }
